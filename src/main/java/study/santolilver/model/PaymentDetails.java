e23ca1b199da7bb7a415571fd895795736ad5288
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-22 16:46:55 -0300

    implementing the model payment class

---
+++ src/main/java/study/santolilver/model/PaymentDetails.java
@@ -0,0 +1,15 @@
+package study.santolilver.model;
+
+public class PaymentDetails {
+
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+}
