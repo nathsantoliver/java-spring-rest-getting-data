468c7bb8ff2e42236b710141d2a2ae18d9ef55be
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-22 16:42:19 -0300

    first commit and publishing the project

---
+++ src/main/java/study/santolilver/SpringRestGettingDataApplication.java
@@ -0,0 +1,13 @@
+package study.santolilver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestGettingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestGettingDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/santolilver/SpringRestGettingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package study.santolilver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestGettingDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
